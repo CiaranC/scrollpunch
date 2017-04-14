f7456c3c0ed08930d423aaa379c7ca451fffeb2a
CiaranC <ciarancanavan@gmail.com>
2017-04-14 10:48:47 +0100

    reset function fixed

--- scrollpunch.js
+++ scrollpunch.js
@@ -3,21 +3,45 @@
 	////trigger = determines element that should be animated when reached 
 	////reset = determines an element that resets the animation 
 	////offset = offsets trigger by a pixel value
+
+	
 	var addClassOnScroll = function(trigger,reset,offset){
+		var hT = $(trigger).offset().top,
+		    wH = window.innerHeight;
+
+			if(hT < wH){
+				if(!$(trigger).hasClass('animate')){
+					trigger.className += ' '+ 'animate';
+				}
+			}
+
+
 		$(window).scroll(function() {
-		   var hT = $(trigger).offset().top,
+
+			var hT = $(trigger).offset().top,
 		       hH = $(trigger).outerHeight(),
-		       wH = $(window).height(),
-		       wS = $(this).scrollTop(),
-		       resetAnimation = $(reset).offset().top;
+		       wH = window.innerHeight;
+		       wS = $(this).scrollTop();
+		       var resetAnimation; 
 
 		   	if(wS >= ((hT-wH)-(offset))){
-		   		$(trigger).addClass('animate');
+
+				if(!$(trigger).hasClass('animate')){
+					trigger.className += ' '+ 'animate';
+				}
+
 		   	}
+
+			if(reset !== 'no-reset'){
+				resetAnimation = $(reset).offset().top;
+				removeAnimation();
+			}
 		
 		   //reset when the distance to page top is less than the distance of reset element to page top
-		   if (wS <= resetAnimation){
-		   	$('.animate').removeClass('animate');
+		   function removeAnimation(){
+			if (wS <= resetAnimation){
+				$(trigger).removeClass('animate');
+			}
 		   }
 		});
 	};
@@ -25,11 +49,15 @@
 	//only fires the above onScroll event if element is present on page - (called on doc ready)
 	var scrollPunch = function(trigger,reset,offset){ 
 		if($(trigger).length>0){
-			$(trigger).each(function(){
-				addClassOnScroll(this,reset,offset);
-			});
+			var triggers = document.body.querySelectorAll(trigger);
+			for(var i = 0; i< triggers.length; i++){
+				var currentTrigger = triggers[i];
+				addClassOnScroll(currentTrigger,reset,offset);
+			}
 		}
 	};
 
 	//trigger animations like this: 
 	// scrollPunch('.my-element','.header','200');
+
+
